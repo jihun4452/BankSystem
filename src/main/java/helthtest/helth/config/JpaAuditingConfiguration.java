f2cc3f722060b66857c38a0142df567615a06786
jihun4452 <jihun6548@naver.com>
2024-07-18 14:04:25 +0900

    회원가입

--- src/main/java/helthtest/helth/config/JpaAuditingConfiguration.java
+++ src/main/java/helthtest/helth/config/JpaAuditingConfiguration.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //Entity 값의 생성 변경시 자동 값 할당
 @Configuration
-
 public class JpaAuditingConfiguration {
 }
