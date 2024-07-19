52769e813fd5dbc961bbac12eda8de8534f08513
jihun4452 <jihun6548@naver.com>
2024-07-19 14:33:46 +0900

    테이블 연결관계

---
+++ src/main/java/helthtest/helth/domain/TransactionEntity.java
@@ -0,0 +1,45 @@
+package helthtest.helth.domain;
+
+import helthtest.helth.type.Transaction;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class TransactionEntity {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private AccountEntity account;
+
+    @Enumerated(EnumType.STRING)
+    private Transaction transaction;
+
+    @NotNull
+    private Long amount;
+
+    private String depositName;
+
+    private String withdrawName;
+
+    private String receivedName;
+
+    private String receivedAccount;
+
+    @CreatedDate
+    private LocalDateTime transactedAt;
+
+
+}
